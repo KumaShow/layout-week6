13868004280ee8d8b92c365849f83b6d3042a21e
KumaShow <kumadth@gmail.com>
2022-08-19 22:58:04 +0800

    新增手機板navMenu按鈕及樣式

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,10 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+const btnMenu = document.querySelector("#btnMenu");
+const phoneMenu = document.querySelector("#phoneMenu");
+const phoneMenuItems = document.querySelectorAll("#phoneMenu>li");
+
+btnMenu.addEventListener('click', ()=> {
+  phoneMenu.classList.toggle('d-none');
+  phoneMenuItems.forEach(function(item,i){
+    item.classList.toggle(`u-animation-fromLeft-${i+1}`);
+  })
+})
