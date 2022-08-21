9feb4bdccf2acffb621cc27ac8e2841c61b8af85
KumaShow <kumadth@gmail.com>
2022-08-21 22:17:19 +0800

    新增手機板menu登入介面效果

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,34 @@
 const btnMenu = document.querySelector("#btnMenu");
 const phoneMenu = document.querySelector("#phoneMenu");
 const phoneMenuItems = document.querySelectorAll("#phoneMenu>li");
+const main = document.querySelector(".main");
+const modalClose = document.querySelector("#modalClose");
+const loginModal = document.querySelector("#loginModal");
 
-btnMenu.addEventListener('click', ()=> {
-  phoneMenu.classList.toggle('d-none');
-  phoneMenuItems.forEach(function(item,i){
-    item.classList.toggle(`u-animation-fromLeft-${i+1}`);
-  })
+let menuShow = false;
+btnMenu.addEventListener("click", ()=> {
+  menuShow = !menuShow;
+  phoneMenu.classList.toggle("d-none");
+  menuAnimationToggle();
+  if (menuShow) {
+    main.addEventListener("click", menuDisplayNone);
+    modalClose.addEventListener("click", menuDisplayNone);
+    loginModal.addEventListener("click", menuDisplayNone);
+  }
 })
+
+function menuDisplayNone() {
+  menuShow = false;
+  phoneMenu.classList.add("d-none");
+  menuAnimationToggle();
+}
+
+function menuAnimationToggle() {
+  phoneMenuItems.forEach(function(item,i){
+    if (menuShow) {
+      item.classList.add(`u-animation-fromLeft-${i+1}`);
+    } else {
+      item.classList.remove(`u-animation-fromLeft-${i+1}`);
+    }
+  })
+}
